0995d7f96aa8e253e2e10f6c5a249041b8fdf38d
Zhao Guangjian <zgj0315@gmail.com>
2024-12-11 11:52:56 +0800

    3d plot works

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,15 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
     use image::{GenericImageView, ImageBuffer, ImageReader};
+    use plotters::{
+        chart::ChartBuilder,
+        prelude::{IntoDrawingArea, IntoLinspace, PathElement, Rectangle, SVGBackend},
+        series::{LineSeries, SurfaceSeries},
+        style::{Color, BLACK, BLUE, GREEN, RED, WHITE},
+    };
 
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
     #[test]
     fn copy_image_pixel_one_by_one() -> anyhow::Result<()> {
         let _ = tracing_subscriber::fmt().with_line_number(true).try_init();
@@ -24,6 +21,162 @@ mod tests {
             *pixel = rgba;
         }
         imgbuf.save("./data/scan_output.png")?;
+        Ok(())
+    }
+
+    #[test]
+    fn plotters_works() -> anyhow::Result<()> {
+        let _ = tracing_subscriber::fmt().with_line_number(true).try_init();
+        let area = SVGBackend::new("./data/3d-plot.svg", (1024, 768)).into_drawing_area();
+
+        area.fill(&WHITE)?;
+
+        let x_axis = (-3.0..3.0).step(0.1);
+        let y_axis = (-30.0..30.0).step(0.1);
+        let z_axis = (-3.0..3.0).step(0.1);
+
+        let mut chart = ChartBuilder::on(&area)
+            .caption("3D Plot Test", ("sans", 20))
+            .build_cartesian_3d(x_axis.clone(), y_axis, z_axis.clone())?;
+
+        chart.with_projection(|mut pb| {
+            pb.yaw = 0.5;
+            pb.scale = 0.9;
+            pb.into_matrix()
+        });
+
+        chart
+            .configure_axes()
+            .light_grid_style(BLACK.mix(0.15))
+            .max_light_lines(3)
+            .draw()?;
+
+        chart
+            .draw_series(
+                SurfaceSeries::xoz(
+                    (-30..30).map(|f| f as f64 / 10.0),
+                    (-30..30).map(|f| f as f64 / 10.0),
+                    |x, z| (x * x + z * z).cos(),
+                )
+                .style(BLUE.mix(0.2).filled()),
+            )?
+            .label("Surface")
+            .legend(|(x, y)| {
+                Rectangle::new([(x + 5, y - 5), (x + 15, y + 5)], BLUE.mix(0.5).filled())
+            });
+
+        chart
+            .draw_series(LineSeries::new(
+                (-100..100)
+                    .map(|y| y as f64 / 40.0)
+                    .map(|y| ((y * 10.0).sin(), y, (y * 10.0).cos())),
+                &BLACK,
+            ))?
+            .label("Line")
+            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], BLACK));
+
+        chart.configure_series_labels().border_style(BLACK).draw()?;
+
+        // To avoid the IO failure being ignored silently, we manually call the present function
+        area.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
+        log::info!("Result has been saved");
+        Ok(())
+    }
+
+    #[test]
+    fn image_rgb_3d_plot() -> anyhow::Result<()> {
+        let _ = tracing_subscriber::fmt().with_line_number(true).try_init();
+        let img = ImageReader::open("./data/scan_raw.jpeg")?.decode()?;
+        let mut x_y_r = HashMap::new();
+        let mut x_y_g = HashMap::new();
+        let mut x_y_b = HashMap::new();
+        let (width, height) = img.dimensions();
+        for x in 0..width {
+            for y in 0..height {
+                let rgba = img.get_pixel(x, y);
+                x_y_r.insert(format!("{}-{}", x, y), rgba.0[0]);
+                x_y_g.insert(format!("{}-{}", x, y), rgba.0[1]);
+                x_y_b.insert(format!("{}-{}", x, y), rgba.0[2]);
+            }
+        }
+
+        let area = SVGBackend::new("./data/3d-plot.svg", (1024, 768)).into_drawing_area();
+
+        area.fill(&WHITE)?;
+
+        let x_axis = (0.0..255.0).step(0.1);
+        let y_axis = (0.0..255.0).step(0.1);
+        let z_axis = (0.0..255.0).step(0.1);
+
+        let mut chart = ChartBuilder::on(&area)
+            .caption("3D Plot Test", ("sans", 20))
+            .build_cartesian_3d(x_axis.clone(), y_axis, z_axis.clone())?;
+
+        chart.with_projection(|mut pb| {
+            pb.yaw = 0.5;
+            pb.scale = 0.9;
+            pb.into_matrix()
+        });
+
+        chart
+            .configure_axes()
+            .light_grid_style(BLACK.mix(0.15))
+            .max_light_lines(3)
+            .draw()?;
+
+        chart
+            .draw_series(
+                SurfaceSeries::xoz(
+                    (0..255).map(|f| f as f64),
+                    (0..255).map(|f| f as f64),
+                    |a, b| {
+                        let z = x_y_r.get(&format!("{}-{}", a, b)).unwrap_or(&0);
+                        *z as f64
+                    },
+                )
+                .style(RED.mix(0.2).filled()),
+            )?
+            .label("Surface")
+            .legend(|(x, y)| {
+                Rectangle::new([(x + 5, y - 5), (x + 15, y + 5)], BLUE.mix(0.5).filled())
+            });
+        chart
+            .draw_series(
+                SurfaceSeries::xoz(
+                    (0..255).map(|f| f as f64),
+                    (0..255).map(|f| f as f64),
+                    |a, b| {
+                        let z = x_y_g.get(&format!("{}-{}", a, b)).unwrap_or(&0);
+                        *z as f64
+                    },
+                )
+                .style(GREEN.mix(0.2).filled()),
+            )?
+            .label("Surface")
+            .legend(|(x, y)| {
+                Rectangle::new([(x + 5, y - 5), (x + 15, y + 5)], BLUE.mix(0.5).filled())
+            });
+
+        chart
+            .draw_series(
+                SurfaceSeries::xoz(
+                    (0..255).map(|f| f as f64),
+                    (0..255).map(|f| f as f64),
+                    |a, b| {
+                        let z = x_y_b.get(&format!("{}-{}", a, b)).unwrap_or(&0);
+                        *z as f64
+                    },
+                )
+                .style(BLUE.mix(0.2).filled()),
+            )?
+            .label("Surface")
+            .legend(|(x, y)| {
+                Rectangle::new([(x + 5, y - 5), (x + 15, y + 5)], BLUE.mix(0.5).filled())
+            });
+
+        // To avoid the IO failure being ignored silently, we manually call the present function
+        area.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
+        log::info!("Result has been saved");
         Ok(())
     }
 }
